21efc232a93ae477f63b553349815f7269604f01
Jennifer Scroggins <jennifer@ravenwilde.com>
2015-05-18 14:59:44 -0400

    individual project templates updated

--- detail.php
+++ detail.php
@@ -1,16 +1,8 @@
-<?php include('perch/runtime.php'); ?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8" />
-	<title><?php perch_pages_title(); ?></title>
-	<?php perch_page_attributes(); ?>
-	<?php perch_get_css(); ?>
-</head>
-<body>
-<!-- detail.php -->
-    <?php perch_pages_navigation(array('levels'=>2)); ?>
-
+<?php 
+	include('perch/runtime.php');   
+	perch_layout('global.header'); 
+  	perch_layout('global.nav');
+  	?>
 	<?php
 	    perch_content_custom('Project', array(
 	      'page' => '/projects.php',
@@ -21,7 +13,4 @@
 	      'count' => 1,
 	    ));
     ?>
-
-    <?php perch_get_javascript(); ?>
-</body>
-</html>
+<?php perch_layout('global.footer'); ?>
