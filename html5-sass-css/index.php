04f63d415b0348614ba436a08b7bc7e70959a4a6
Jennifer Scroggins <jennifer@ravenwilde.com>
2015-06-02 13:57:09 -0400

    assets

---
+++ html5-sass-css/index.php
@@ -0,0 +1,35 @@
+<?php 
+	include($_SERVER['DOCUMENT_ROOT'].'/perch/runtime.php'); 
+	perch_layout('global.header', array(
+		'body-class'=>'skill',
+	)); 
+	perch_layout('global.nav');
+	?>
+    <header>
+    	<div class="container">
+		    <h1><?php perch_content('Main Header'); ?></h1>
+		</div>
+	</header>
+	<section>
+		<div class="container">
+		    <?php perch_content('Intro'); ?>
+			<?php perch_content('Gist Links'); ?>		
+		</div>
+	</section>
+	<section>
+		<div class="container">
+		    <?php 
+				perch_content_create('Skill', array(
+			      'template'  => 'category_skill.html',
+			      'edit-mode' => 'singlepage',
+					));
+
+				perch_content_custom('Project', array(
+			        'template' => 'project_listing.html',
+			        'page'=>'/projects.php',
+			        'category' => 'skills/sasscss',
+		      		));
+		    ?>
+		</div>
+	</section>
+<?php perch_layout('global.footer'); ?>
