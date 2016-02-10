f9cb39b9f7a4305dcbc8c1bb264ae06aaa7a9858
Jennifer Scroggins <jennifer@ravenwilde.com>
2016-02-10 14:36:04 -0500

    Homepage Typography

--- perch/templates/layouts/global.footer.php
+++ perch/templates/layouts/global.footer.php
@@ -8,6 +8,7 @@
 </div>
 <footer>
 	<div class="container">
+		<h4>This website powered by PerchCMS, written in PHP, HTML5, &amp; Sass w/Bourbon &amp; Neat.</h4>
 		<h5>jennifer.codes &copy; 2015 - Jennifer A. Scroggins</h5>
 	</div>
 </footer>
--- perch/templates/layouts/global.nav.php
+++ perch/templates/layouts/global.nav.php
@@ -1,11 +1,11 @@
 <nav class="global-nav">
 	<div class="container">
     	<?php perch_pages_navigation(array('levels'=>1)); ?>
+    	<a href="#" class="menu-toggle">-></a>
 	</div>
 </nav>
 <nav class="global-nav-skills">
 	<div class="container">
-		<a href="#" class="menu-toggle">Skills -></a>
 		<?php 
 			perch_pages_navigation(array(
 				  'navgroup' =>'skills',
